5879a7c6de05fe20a0cbffb054371cc11c801d44
Anjr0o <84.andrew.stokes@gmail.com>
2020-10-14 12:58:07 -0500

    Updates

---
+++ precache-manifest.d83ba104b59ff987267614264a0a1eae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d453bc74482085ad4f8c23ef87919809",
+    "url": "/laptop-customizer/index.html"
+  },
+  {
+    "revision": "dc9810b2e2e3c081120d",
+    "url": "/laptop-customizer/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "0801e985a08028591842",
+    "url": "/laptop-customizer/static/js/2.ed960d5b.chunk.js"
+  },
+  {
+    "revision": "dc9810b2e2e3c081120d",
+    "url": "/laptop-customizer/static/js/main.c68832f8.chunk.js"
+  },
+  {
+    "revision": "499f4b4adf8f43d881a8",
+    "url": "/laptop-customizer/static/js/runtime~main.9cc997fb.js"
+  }
+]);
